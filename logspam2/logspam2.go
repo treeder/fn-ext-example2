2e98c4326ee033727de61cb3762b4577be6e9a8e
Travis Reeder <treeder@gmail.com>
2018-01-09 17:50:11 -0800

    Fixed for recent fn changes.

--- logspam2/logspam2.go
+++ logspam2/logspam2.go
@@ -5,13 +5,26 @@ import (
 	"fmt"
 
 	"github.com/fnproject/fn/api/models"
+	"github.com/fnproject/fn/api/server"
+	"github.com/fnproject/fn/fnext"
 	"github.com/treeder/yolib"
 )
 
-// This implements the extender.CallListener interface
+func init() {
+	server.RegisterExtension(&LogSpam{})
+}
+
 type LogSpam struct {
 }
 
+func (e *LogSpam) Name() string {
+	return "github.com/treeder/fn-ext-example2/logspam2"
+}
+
+func (e *LogSpam) Setup(s fnext.ExtServer) error {
+	s.AddCallListener(&LogSpam{})
+	return nil
+}
 func (l *LogSpam) BeforeCall(ctx context.Context, call *models.Call) error {
 	fmt.Println("LOGSPAM2!!! LOGSPAM2!!! LOGSPAM2!!! LOGSPAM2!!!")
 	yolib.Call()
